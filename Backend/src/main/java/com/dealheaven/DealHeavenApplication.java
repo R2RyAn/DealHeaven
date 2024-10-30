77db447fd55b657691d9ab8ded9055e7be86e746
Rayan AL Dajani <92130344+R2RyAn@users.noreply.github.com>
2024-10-30 01:45:25 -0400

    User and Post Services and Controllers are fully functional Fixed minor bugs

---
+++ Backend/src/main/java/com/dealheaven/DealHeavenApplication.java
@@ -0,0 +1,32 @@
+package com.dealheaven;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class DealHeavenApplication {
+
+    public static void main(String[] args) throws IOException {
+        // Check if FirebaseApp has already been initialized
+        if (FirebaseApp.getApps().isEmpty()){
+            FileInputStream serviceAccount =
+                    new FileInputStream("src/main/resources/serviceAccountKey.json");
+
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+
+        }
+
+        SpringApplication.run(DealHeavenApplication.class, args);
+    }
+}
