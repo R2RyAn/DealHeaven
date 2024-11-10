693cdd2eeaec65bcc8f712618bfafa0613d5f603
Rayan AL Dajani <92130344+R2RyAn@users.noreply.github.com>
2024-11-09 20:14:25 -0500

    Created PostMapping /create in Review Controller

--- Backend/src/main/java/com/dealheaven/controllers/ReviewController.java
+++ Backend/src/main/java/com/dealheaven/controllers/ReviewController.java
@@ -1,14 +1,30 @@
 package com.dealheaven.controllers;
 
 
+import com.dealheaven.models.Review;
+import com.dealheaven.models.User;
 import com.dealheaven.services.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.ExecutionException;
+
 @RestController
-@RequestMapping("/reviews")
+@RequestMapping("/review")
 public class ReviewController {
+
     @Autowired
     private ReviewService reviewService;
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createReview(@RequestBody Review review) throws ExecutionException, InterruptedException {
+        ReviewService.createReview(review);
+        return ResponseEntity.ok("User created successfully with id: " + review.getId());
+    }
+
+
 }
