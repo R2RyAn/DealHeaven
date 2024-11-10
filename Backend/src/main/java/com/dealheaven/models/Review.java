4dc341f6319361f52c7c75fbfc1659dca5623d89
Rayan AL Dajani <92130344+R2RyAn@users.noreply.github.com>
2024-11-09 20:17:49 -0500

    created first service for Review Controller as well as fixed Review Model

--- Backend/src/main/java/com/dealheaven/models/Review.java
+++ Backend/src/main/java/com/dealheaven/models/Review.java
@@ -1,7 +1,12 @@
 package com.dealheaven.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Date;
 
+@Setter
+@Getter
 public class Review {
     private String id;
     private String postId;
--- Backend/src/main/java/com/dealheaven/services/ReviewService.java
+++ Backend/src/main/java/com/dealheaven/services/ReviewService.java
@@ -1,8 +1,44 @@
 package com.dealheaven.services;
 
 
+import com.dealheaven.models.Review;
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.*;
+import com.google.firebase.cloud.FirestoreClient;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 @Service
 public class ReviewService {
+
+    private static final String REVIEW_COLLECTION = "reviews";
+
+    public void createReview(Review review) throws ExecutionException, InterruptedException {
+        Firestore db = FirestoreClient.getFirestore();
+        CollectionReference users = db.collection(REVIEW_COLLECTION);
+
+        ApiFuture<QuerySnapshot> emailQuery = users.whereEqualTo("comment", review.getComment()).get();
+        List<QueryDocumentSnapshot> emailDocuments = emailQuery.get().getDocuments();
+        ApiFuture<QuerySnapshot> phoneQuery = users.whereEqualTo("rating", review.getRating()).get();
+        List<QueryDocumentSnapshot> phonedDocuments = phoneQuery.get().getDocuments();
+
+        if(!emailDocuments.isEmpty()) {
+            throw new IllegalArgumentException("A user with this email already exists.");
+        }
+
+        if(!phonedDocuments.isEmpty()) {
+            throw new IllegalArgumentException("A user with this email already exists.");
+        }
+
+
+        // Add user and retrieve the document reference
+        DocumentReference documentReference = users.add(review).get();
+        // Set the Firebase-generated ID to the user object
+        String generatedId = documentReference.getId();
+        review.setId(generatedId);
+        // Optionally update the user document with the generated ID
+        documentReference.set(review).get();
+    }
 }
