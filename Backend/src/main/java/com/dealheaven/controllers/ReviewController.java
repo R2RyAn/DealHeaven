db08f36dd5940531cbfd7517a61e2c2a1bfbfaf9
Rayan AL Dajani <92130344+R2RyAn@users.noreply.github.com>
2024-11-09 20:10:48 -0500

    set up initial for Review controller

--- Backend/src/main/java/com/dealheaven/controllers/ReviewController.java
+++ Backend/src/main/java/com/dealheaven/controllers/ReviewController.java
@@ -1,4 +1,14 @@
 package com.dealheaven.controllers;
 
+
+import com.dealheaven.services.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/reviews")
 public class ReviewController {
+    @Autowired
+    private ReviewService reviewService;
 }
