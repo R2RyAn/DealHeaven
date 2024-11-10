a80f1e20241da548b7fbcfaf980948397e613edb
Rayan AL Dajani <92130344+R2RyAn@users.noreply.github.com>
2024-11-09 20:21:11 -0500

    new Get mapping to get Review By Id

--- Backend/src/main/java/com/dealheaven/controllers/ReviewController.java
+++ Backend/src/main/java/com/dealheaven/controllers/ReviewController.java
@@ -6,10 +6,7 @@ import com.dealheaven.models.User;
 import com.dealheaven.services.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.ExecutionException;
 
@@ -24,6 +21,12 @@ public class ReviewController {
     public ResponseEntity<?> createReview(@RequestBody Review review) throws ExecutionException, InterruptedException {
         ReviewService.createReview(review);
         return ResponseEntity.ok("User created successfully with id: " + review.getId());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Review> getReviewById(@PathVariable String id) throws ExecutionException, InterruptedException {
+        Review review = reviewService.getReviewById(id);
+        return ResponseEntity.ok(review);
     }
 
 
